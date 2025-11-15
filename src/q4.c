4bac03057a2a9b98d4013dafc9c04ef6e319eb10
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2025-11-15 13:16:14 +0545

    Update q4.c

--- src/q4.c
+++ src/q4.c
@@ -1,1 +1,20 @@
-// Implement a C program to find the second largest element in an array.
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++) scanf("%d", &arr[i]);
+
+    int max = arr[0], second = arr[0];
+    for(int i=1; i<n; i++) {
+        if(arr[i] > max) {
+            second = max;
+            max = arr[i];
+        } else if(arr[i] > second && arr[i] != max) {
+            second = arr[i];
+        }
+    }
+    printf("Second largest element: %d\n", second);
+    return 0;
+}
