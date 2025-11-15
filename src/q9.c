27ea909aa0c894d81cf31195c7313e55b30a3aca
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2025-11-15 13:18:58 +0545

    Update q9.c

--- src/q9.c
+++ src/q9.c
@@ -1,1 +1,16 @@
-// Implement a C program to count the occurrence of a specific character in a string.
+#include <stdio.h>
+int main() {
+    char str[100], ch;
+    printf("Enter a string: ");
+    scanf("%s", str);
+    printf("Enter a character to count: ");
+    scanf(" %c", &ch);
+
+    int count = 0, i = 0;
+    while(str[i] != '\0') {
+        if(str[i] == ch) count++;
+        i++;
+    }
+    printf("Character '%c' occurs %d times\n", ch, count);
+    return 0;
+}
