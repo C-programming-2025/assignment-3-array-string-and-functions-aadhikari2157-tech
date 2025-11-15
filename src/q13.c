bf4e33607c2ca3b5ce78affcc563b10242051c21
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2025-11-15 13:20:05 +0545

    Update q13.c

--- src/q13.c
+++ src/q13.c
@@ -1,1 +1,12 @@
-// Write a function named findFactorial that takes an integer as input and returns its factorial.
+#include <stdio.h>
+long long findFactorial(int n) {
+    long long fact = 1;
+    for(int i=1; i<=n; i++) fact *= i;
+    return fact;
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("Factorial: %lld\n", findFactorial(n));
+    return 0;
+}
