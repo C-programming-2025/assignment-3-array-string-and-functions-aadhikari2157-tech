ac2346fb2c4dc35f69a956953c91ce4e315cee43
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2025-11-15 13:03:16 +0545

    Update q1.c

--- src/q1.c
+++ src/q1.c
@@ -1,1 +1,16 @@
-// Write a C program to find the maximum and minimum elements in an array.
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++) scanf("%d", &arr[i]);
+
+    int max = arr[0], min = arr[0];
+    for(int i=1; i<n; i++) {
+        if(arr[i] > max) max = arr[i];
+        if(arr[i] < min) min = arr[i];
+    }
+    printf("Maximum: %d\nMinimum: %d\n", max, min);
+    return 0;
+}
