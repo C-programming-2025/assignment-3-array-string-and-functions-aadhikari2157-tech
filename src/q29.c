3ae5cda9bf809ed374b3d6f06564671256fa2c80
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2025-11-15 13:35:34 +0545

    Update q29.c

--- src/q29.c
+++ src/q29.c
@@ -1,1 +1,21 @@
-// Write a recursive function named isPalindrome that takes a string as input and returns 1 if it is a palindrome (reads the same forwards and backwards), and 0 otherwise.
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    int len = strlen(str);
+    int flag = 1;
+    for(int i = 0; i < len/2; i++) {
+        if(str[i] != str[len - 1 - i]) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if(flag) printf("Palindrome");
+    else printf("Not Palindrome");
+
+    return 0;
+}
