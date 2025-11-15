7d956c0da5ec0921823c2e3e3cb7f1e99241de6e
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2025-11-15 13:18:10 +0545

    Update q6.c

--- src/q6.c
+++ src/q6.c
@@ -1,1 +1,12 @@
-// Write a C program to find the length of a string without using the built-in string functions.
+#include <stdio.h>
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    int length = 0;
+    while(str[length] != '\0') length++;
+
+    printf("Length of string: %d\n", length);
+    return 0;
+}
