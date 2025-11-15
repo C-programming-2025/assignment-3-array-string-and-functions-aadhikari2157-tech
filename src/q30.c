0ea6c60cdfdb5bcccf2165e3f2b050fbaa674010
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2025-11-15 13:35:54 +0545

    Update q30.c

--- src/q30.c
+++ src/q30.c
@@ -1,1 +1,17 @@
-//  Write a recursive function named countOccurrences that takes a string and a character as input and returns the number of times the character appears in the string.
+#include <stdio.h>
+int main() {
+    char str[100], ch;
+    printf("Enter a string: ");
+    scanf("%s", str);
+    printf("Enter a character to count: ");
+    scanf(" %c", &ch);
+
+    int count = 0;
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(str[i] == ch)
+            count++;
+    }
+
+    printf("Character '%c' occurs %d times", ch, count);
+    return 0;
+}
