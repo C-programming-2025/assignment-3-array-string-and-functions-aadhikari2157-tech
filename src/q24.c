6f01809525a4e77748761d33c9511b9fc68a3677
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2025-11-15 13:33:38 +0545

    Update q24.c

--- src/q24.c
+++ src/q24.c
@@ -1,1 +1,13 @@
-// Write a recursive function named calculatePower that takes two integers base and exponent as input and returns the result of raising the base to the exponent.
+#include <stdio.h>
+int main() {
+    int base, exp;
+    printf("Enter base and exponent: ");
+    scanf("%d %d", &base, &exp);
+
+    int result = 1;
+    for(int i = 1; i <= exp; i++)
+        result *= base;
+
+    printf("%d^%d = %d", base, exp, result);
+    return 0;
+}
