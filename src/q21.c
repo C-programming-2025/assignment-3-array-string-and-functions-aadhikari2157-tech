afda891b04596140e7d32ba2ff8744e7e5625d3f
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2025-11-15 13:32:49 +0545

    Update q21.c

--- src/q21.c
+++ src/q21.c
@@ -1,1 +1,13 @@
-// Write a recursive function named calculateFactorial that takes an integer n as input and returns its factorial.
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    long long fact = 1;
+    for(int i = 1; i <= n; i++)
+        fact *= i;
+
+    printf("Factorial = %lld", fact);
+    return 0;
+}
